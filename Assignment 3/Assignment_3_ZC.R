163cf8cc7fa3755e76f8075a169a47f72e619374
hasana07 <77554611+hasana07@users.noreply.github.com>
2024-07-04 22:23:18 -0400

    Update Assignment_3_ZC.R
    added review comments

--- Assignment 3/Assignment_3_ZC.R
+++ Assignment 3/Assignment_3_ZC.R
@@ -2,6 +2,11 @@
 #' R version: Version 2024.04.2+764 (2024.04.2+764)
 #' Code dated to: June 30
 
+#HA (Code review lines will start with HA-Hasan Abdo): It seems like you've started by creating "screen" functions that will update an actual figure of a hangman depending on user input. Smart!
+#HA: I would sugest you add more comments (for a reviewer like me) to understand the flow of your logic better/
+#HA: Great start however
+
+
 #' Setting functions for use in the script
 screen_1 <- function() {
   # Using cat here for better displaying than print() as "/n" helps to format the message
@@ -23,6 +28,10 @@ screen_1 <- function() {
       "  3. Every word is related to every-day items & actvities, thats the only hint you get! \n", 
       "  4. Each game guesses the identity of ONE word (no dashes). \n",
       "  5. When you're out of lives, you lose!")
+  
+#HA: really like how you lay out the rules from the start, I didn't add this in my game, but I like your approach here!
+  
+  
 } # The other functions for displaying screens follow a similar logic/structure
 
 # Function for displaying screen when lost 1 life 
@@ -196,11 +205,15 @@ screenC_5 <- function() {
   cat(blank_vector, "\n")
 }
 
+#HA: Great stuff! I followed the "screens" and it does seem to flow logically. 
+
 
 # Function for capturing user input 
 user_enter <- function() {
   readline(prompt = "Enter your guess: ")
 }
+
+#HA: great, maybe tell the user to enter a single letter here as well? 
 
 # Function for displaying screen when winning the game
 congrats <- function() {
@@ -217,13 +230,18 @@ congrats <- function() {
   print(paste("You did it! The secret word was: ", word))
 }
 
+#HA: Very creative! I really like this idea, adds a personal touch to your game. Makes it more visually appealing. Great stuff!
+
 
 # --------------------------------------------------------------------------------
 # Actual code begins:
 
 # Defining file path to txt file for list of words
-file_path <- "/Users/zachery/BTC1855/Assignment 3/ListofWords.txt"
+file_path <- "~/Documents/BTC1855 Coding in R/ListofWords.txt"
 # Please adjust this code to match where you will download the txt file
+
+#HA: I like how you are notifying a reviewer to adjust their working directory here, mayne illustrate more for clarity. 
+
 
 # Calling word list using readlines(), used suppressWarnings() to disregard message about incomplete final line in txt file
 word_list <- suppressWarnings(readLines(file_path))
@@ -240,6 +258,8 @@ lives <- 6
 
 # creating blank vector for displaying the word & correct guesses, repeating "_" as fillers 
 blank_vector <- c(rep("_", word_length))
+
+#HA: Nice! Adds a visual cue for the User
 
 # Create vector to store incorrect guesses 
 incorrect_letters <- c()
@@ -261,11 +281,16 @@ screen_1()
 #` -----------------------------------------------------------------------------------`
 
 #' Using while loop to ask for user input as long as lives =/= 0  
+
+#HA: Good use of the while loop!
 while (lives > 0) {
   # Calling user_enter() function (defined earlier), using toupper() to disregard typecase like in the txt file
   input <- toupper(user_enter())
   # Set default to no match 
   match <- F
+
+#HA: I think here, you need to elaborate more on what "match" is doing.
+  
   
   # Using for loop to compare single character input to all elements in split_word while keeping index count 
   for (i in seq_along(split_word)) {
@@ -277,10 +302,15 @@ while (lives > 0) {
     }
   }
   
+  #HA: great, so this is placing a correct guess within the correct position of the split word. Good stuff. 
+  
   # if match = TRUE, print dashes (and correct), and congratulate 
   if (match) {
     print(blank_vector)
     print(paste("Correct! Keep going! You have", lives, "lives left"))
+  
+#HA: Now i understand the purpose of "match", however, I think you need to comment on this before hand so it is easier to follow through.
+    
     
     #Using if-else statements to display current artwork for lives when correct 
     if (lives == 6) {
@@ -290,15 +320,18 @@ while (lives > 0) {
     } else if (lives == 4) {
       screenC_3()
     } else if (lives == 3) {
-     screenC_4()
+      screenC_4()
     } else if (lives == 2) {
       screenC_5()
     } else if (lives == 1) {
       screenC_6()
     }
+    
+  #HA: Oh I like this bit here! So elegant, great job!  
+
   } else {
     # IF user is incorrect (match = FALSE), use of else here computes this code 
-    
+  
     # Redefine lives (reduce by 1)
     lives <- lives - 1 
     
@@ -321,8 +354,8 @@ while (lives > 0) {
     } else if ( lives == 0) {
       screen_7()
       break ()
-      } 
-   }
+    } 
+  }
   # If statement to display screen upon winning 
   if (identical(blank_vector, split_word)) {
     congrats()
@@ -330,3 +363,6 @@ while (lives > 0) {
   }
 } # Closes while loop 
 
+
+#HA: Overall, amazing job Zach! Great use of visuals and I felt the script had logical flow. One thing I would recommend is to add more comments, so that a reviewer (like me)
+#- could easily follow along your logic/code. The game runs smoothly, functions work without errors, amaizng job once again!
